26e046145450098ee0ea4e1107f6ccc5508746ab
Steven Lehrburger <lehrburger@gmail.com>
2012-06-02 16:16:25 -0400

    Re-add dev env_vars to work w/ try-except import

---
+++ scripts/config-dev/secrets.py
@@ -0,0 +1,22 @@
+component_port = 5237
+client_port = 5222
+xmlrpc_port = 4560
+
+server = 'localhost'
+repo_dir = '/vagrant/chatidea/'
+log_dir = '/var/log/chatidea/'
+
+proxybot_password = 'ow4coirm5oc5coc9folv'
+hostbot_secret = 'is3joic8vorn8uf4ge4o'
+default_user_password = 'password'
+
+admin_users = ['%s@%s' % (user, server) for user in ['admin1', 'admin2']]
+admin_password = 'FgT5bk3'
+
+hostbot_xmlrpc_password = 'wraf7marj7og4e7ob4je'
+proxybot_xmlrpc_password = 'floif8ef7ceut5yek4da'
+rosterbot_xmlrpc_password = 'nal4rey2hun5ewv4ud6p'
+
+hostbot_mysql_password = 'ish9gen8ob8hap7ac9hy'
+proxybotinfo_mysql_password = 'oin9yef4aim9nott8if9'
+userinfo_mysql_password = 'me6oth8ig3tot7as2ash'
