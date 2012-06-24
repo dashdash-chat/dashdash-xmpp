79b98f0a18fb3a49abf43bbaaaeacfd2b4c2541e
Steven Lehrburger <lehrburger@gmail.com>
2012-06-23 20:04:21 -0400

    Clean up constants files, remove unused bot files

--- scripts/config_dev/env_vars.py
+++ scripts/config_dev/env_vars.py
@@ -1,28 +1,14 @@
-leaf_secret = 'is3joic8vorn8uf4ge4o'
-leaf_mysql_password = 'ish9gen8ob8hap7ac9hy'
-leaf_xmlrpc_password = 'wraf7marj7og4e7ob4je'
-
+server = 'dev.vine.im'
+server_ip = '127.0.0.1'
 
 component_port = 5237
 client_port = 5222
 xmlrpc_port = 4560
 
-server = 'dev.vine.im'
-server_ip = '127.0.0.1'
-repo_dir = '/vagrant/chatidea/'
-log_dir = '/var/log/chatidea/'
-
-proxybot_password = 'ow4coirm5oc5coc9folv'
-hostbot_secret = 'is3joic8vorn8uf4ge4o'
-default_user_password = 'password'
+leaf_secret = 'is3joic8vorn8uf4ge4o'
+leaf_xmlrpc_password = 'wraf7marj7og4e7ob4je'
+leaf_mysql_password = 'ish9gen8ob8hap7ac9hy'
 
 admin_users = ['%s@%s' % (user, server) for user in ['admin1', 'admin2']]
-admin_password = 'FgT5bk3'
 
-hostbot_xmlrpc_password = 'wraf7marj7og4e7ob4je'
-proxybot_xmlrpc_password = 'floif8ef7ceut5yek4da'
-rosterbot_xmlrpc_password = 'nal4rey2hun5ewv4ud6p'
-
-hostbot_mysql_password = 'ish9gen8ob8hap7ac9hy'
-proxybotinfo_mysql_password = 'oin9yef4aim9nott8if9'
-userinfo_mysql_password = 'me6oth8ig3tot7as2ash'
+default_user_password = 'password'
--- scripts/constants.py
+++ scripts/constants.py
@@ -5,73 +5,24 @@ except ImportError, e:
 import shortuuid
 shortuuid.set_alphabet('1234567890abcdefghijklmnopqrstuvwxyz')
 
+server = env_vars.server
+server_ip = env_vars.server_ip
+
+component_port = env_vars.component_port
 leaf_name = "leaf"
-leaf_secret = env_vars.hostbot_secret
-leaf_mysql_password = env_vars.leaf_mysql_password
-db_name = 'vine'
-vinebot_prefix = 'vinebot_'
+leaf_secret = env_vars.leaf_secret
+
+xmlrpc_port = env_vars.xmlrpc_port
 leaf_xmlrpc_jid_prefix  = '_leaf'
 leaf_xmlrpc_password = env_vars.leaf_xmlrpc_password
+
+leaf_mysql_password = env_vars.leaf_mysql_password
+db_name = 'vine'
+
+vinebot_prefix = 'vinebot_'
 roster_group = 'Vine Conversations'
 
 admin_users = env_vars.admin_users
 
-
-
-server = env_vars.server
-server_ip = env_vars.server_ip
-component_port = env_vars.component_port
 client_port = env_vars.client_port
-xmlrpc_port = env_vars.xmlrpc_port
-
-proxybot_script = '%sscripts/proxybot_client.py' % env_vars.repo_dir
-proxybot_prefix = 'proxybot_'
-proxybot_resource = 'python_client'
-proxybot_password = env_vars.proxybot_password
-hostbot_user = 'host'
-hostbot_server = 'bot.%s' % server
-hostbot_user_jid = '%s@%s' % (hostbot_user, hostbot_server)
-hostbot_component_jid = '%s/python_component' % hostbot_server
-hostbot_nick = 'Hostbot'
-hostbot_secret = env_vars.hostbot_secret
 default_user_password = env_vars.default_user_password
-
-admin_password = env_vars.admin_password
-
-hostbot_xmlrpc_jid = '_hostbot'
-hostbot_xmlrpc_password = env_vars.hostbot_xmlrpc_password
-proxybot_xmlrpc_jid = '_proxybot'
-proxybot_xmlrpc_password = env_vars.proxybot_xmlrpc_password
-rosterbot_xmlrpc_jid = '_rosterbot'
-rosterbot_xmlrpc_password = env_vars.rosterbot_xmlrpc_password
-
-proxybot_group = 'contacts'
-active_group = 'Vine Conversations'
-idle_group = 'Vine Contacts'
-
-hostbot_mysql_user = 'hostbot'
-hostbot_mysql_password = env_vars.hostbot_mysql_password
-proxybotinfo_mysql_user = 'proxybotinfo'
-proxybotinfo_mysql_password = env_vars.proxybotinfo_mysql_password
-userinfo_mysql_user = 'userinfo'
-userinfo_mysql_password = env_vars.userinfo_mysql_password
-
-proxybot_logfile = '%sproxybots.log' % env_vars.log_dir
-
-class Stage:
-    IDLE = 1
-    ACTIVE = 2
-    RETIRED = 3
-
-class ProxybotCommand:
-    activate = 'activate'
-    retire = 'retire'
-    add_participant = 'add_participant'
-    remove_participant = 'remove_participant'
-
-class HostbotCommand:
-    delete_proxybot = 'delete_proxybot'
-    bounce_proxybot = 'bounce_proxybot'
-    participant_deleted = 'participant_deleted'
-    add_observer = 'add_observer'
-    remove_observer = 'remove_observer'
