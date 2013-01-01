fe5bb01801f717eb891dac6cb2c587807a31b251
Steven Lehrburger <lehrburger@gmail.com>
2013-01-01 17:45:35 -0500

    Basic cookbook for vine_xmpp

---
+++ cookbooks/vine_xmpp/metadata.rb
@@ -0,0 +1,9 @@
+maintainer       "Vine.IM"
+maintainer_email "lehrburger@gmail.com"
+license          "All rights reserved"
+description      "Installs/Configures vine_xmpp"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+
+#NOTE using specific versions so that I can stay aware of changes in upstream cookbooks
+depends "nginx", "= 1.1.2"
