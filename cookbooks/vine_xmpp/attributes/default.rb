fb50aa6de7bcab998ebe37942917a6acbc92503d
Steven Lehrburger <lehrburger@gmail.com>
2013-01-02 03:40:40 -0500

    Clean up attribute names

--- cookbooks/vine_xmpp/attributes/default.rb
+++ cookbooks/vine_xmpp/attributes/default.rb
@@ -1,2 +1,2 @@
-default['vine_xmpp']['xmpp_venv_dir']     = "#{Chef::Environment.load(node.chef_environment).default_attributes['dirs']['source_dir']}/xmpp-env"
-default['vine_xmpp']['xmpp_repo_dir']     = "#{node['vine_xmpp']['xmpp_venv_dir']}/xmpp"
+default['vine_xmpp']['xmpp_env_dir']  = "#{Chef::Environment.load(node.chef_environment).default_attributes['dirs']['source']}/xmpp-env"
+default['vine_xmpp']['xmpp_repo_dir'] = "#{node['vine_xmpp']['xmpp_env_dir']}/xmpp"
