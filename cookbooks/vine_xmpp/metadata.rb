d6ff892f172cd5bfaeed6b2404e82a8d2b6c15e6
Steven Lehrburger <lehrburger@gmail.com>
2013-01-11 19:33:16 -0500

    Set cookbook version, specify more dependencies

--- cookbooks/vine_xmpp/metadata.rb
+++ cookbooks/vine_xmpp/metadata.rb
@@ -3,8 +3,8 @@ maintainer_email "lehrburger@gmail.com"
 license          "All rights reserved"
 description      "Installs/Configures vine_xmpp"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "1.0.0"
 
-#NOTE using specific versions so that I can stay aware of changes in upstream cookbooks
-depends "vine_shared", "~> 0.1.0"
 depends "deploy_wrapper", "= 0.0.2"
+depends "vine_shared", ">= 1.0.0"
+depends "vine_ejabberd", ">= 1.0.0"
