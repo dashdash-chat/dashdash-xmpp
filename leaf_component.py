3a1d9d6e06b7be189d59ff6dfdbf55659e89275b
Steven Lehrburger <lehrburger@gmail.com>
2013-04-29 14:21:02 -0400

    Remove chatstates from observers
    I think it already sends chatstates to all observers, even those that are offline, which is fine, but then if they're offline they'll send back errors and I was then trying to broadcast those errors, which was causing the leaf to do way too much work. At least, that's what it seemed like, I'm not totally sure, and those chatstates aren't important enough to be worth investigating.

--- leaf_component.py
+++ leaf_component.py
@@ -622,9 +622,6 @@ class LeafComponent(ComponentXMPP):
             del msg['type']
             if user in vinebot.participants:
                 self.broadcast_message(vinebot, user, vinebot.everyone.difference([user]), None, msg=msg)
-            elif user in vinebot.observers:
-                msg.error()  #TODO figure out why, without this one line, it broadcasts your own chat state back to you in a two person conversation with helpbot
-                self.broadcast_message(vinebot, user, vinebot.participants, None, msg=msg)
             elif user in vinebot.edge_users:
                 self.broadcast_message(vinebot, user, vinebot.edge_users.difference([user]), None, msg=msg)
         except NotVinebotException:
